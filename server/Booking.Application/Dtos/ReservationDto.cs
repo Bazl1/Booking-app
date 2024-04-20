1186ea21979750cbaf282d83fa4891e79f94fec0
Fernir Team <rushhourteam@fernir.co>
2024-04-20 16:13:18 +0300

    fix: reservation dto

--- server/Booking.Application/Dtos/ReservationDto.cs
+++ server/Booking.Application/Dtos/ReservationDto.cs
@@ -7,7 +7,7 @@ public class ReservationDto
     public string Id { get; set; }
     public string Title { get; set; }
     public string Poster { get; set; }
-    public UserDto User { get; set; }
+    public UserDto Author { get; set; }
     public string StartDate { get; set; }
     public string EndDate { get; set; }
     public int NumberOfDays { get; set; }
